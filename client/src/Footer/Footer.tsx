2aeb336f9fa53f959c3d965cf715b921969d28a3
Servaas Phlips <servi@telenet.be>
2022-03-20 12:56:17 +0100

    - fixed some typo's - added location of code in the footer

--- client/src/Footer/Footer.tsx
+++ client/src/Footer/Footer.tsx
@@ -8,7 +8,8 @@ const Footer = () => {
         <div className='is-flex is-align-content-center is-justify-content-center'>
           <div className='is-flex is-align-items-baseline'><Icon name='info' size='small' />Online C.V. Servaas Phlips</div>
           <div className='is-flex is-align-items-baseline ml-3'><Icon name='calendar-month' size='small' /> 2022</div>
-          <div className='is-flex is-align-items-baseline ml-3'><Icon name='construction' size='small' /> React, NodeJS (Express) and MongoDb</div>
+          <div className='is-flex is-align-items-baseline ml-3'><Icon name='construction' size='small' />React, Redux, NodeJS (Express) & MongoDb</div>
+          <div className='is-flex is-align-items-baseline ml-3'><Icon name='account-tree' size='small' /><a href="https://github.com/serviax/cv">Code repository</a></div>
         </div>
       </div>
     </footer>
